c3ab7051104e3d443375a32347674caa5f5a2a4a
Adam D. Carthan <Adamcarthan55@gmail.com>
2025-09-07 16:39:05 -0400

    Add Booking API with ViewSet and router

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
